2ab2e9dc71fdc992feb453bf16e18d36a0f77d15
unknown <divyacharaninabha@gmail.com>
2020-11-12 20:14:55 +0530

    Added : Functionality to search contacts by city state in addressbook added

--- AddressBook.js
+++ AddressBook.js
@@ -1,42 +1,5 @@
 const prompt = require('prompt-sync')();
-let nameRegex = RegExp("^[A-Z][Aa-zZ]{2,}$");
-let addressRegex = RegExp("^[A-Za-z0-9,/]{4,}$");
-let cityStateRegex = RegExp("^[A-Za-z]{4,}$");
-let zipRegex = RegExp("^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$");
-let phoneNumberRegex = RegExp("^[1-9]{1}[0-9]{9}$");
-let emailRegex = RegExp("[\\w+_-]+(?:\\.[\\w_-]+)*@(?:[\\w]+\\.){1,2}[a-zA-Z]{2,}");
-class Contact {
-    constructor(...params) {
-        if (nameRegex.test(params[0]))
-            this.firstName = params[0];
-        else throw "Invalid First Name!!";
-        if (nameRegex.test(params[1]))
-            this.lastName = params[1];
-        else throw "Invalid Last Name!!";
-        if (addressRegex.test(params[2]))
-            this.address = params[2];
-        else throw "Invalid Address!!";
-        if (cityStateRegex.test(params[3]))
-            this.city = params[3];
-        else throw "Invalid City!!";
-        if (cityStateRegex.test(params[4]))
-            this.state = params[4];
-        else throw "Invalid State!!";
-        if (zipRegex.test(params[5]))
-            this.zip = params[5];
-        else throw "Invalid Zip!!";
-        if (phoneNumberRegex.test(params[6]))
-            this.phoneNumber = params[6];
-        else throw "Invalid Phone Number!!";
-        if (emailRegex.test(params[7]))
-            this.email = params[7];
-        else throw "Invalid Email!!";
-    }
-
-    toString() {
-        return "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", Address : " + this.address + ", City : " + this.city + ", State : " + this.state + ", Zip : " + this.zip + ", Phone Number : " + this.phoneNumber + ", Email : " + this.email;
-    }
-}
+const Contact = require('./Contact.js')
 
 let addressBookArr = new Array();
 
@@ -104,10 +67,20 @@ let deleteContact = () => {
     }
 }
 
+let searchByCity = () => {
+    let searchCity = prompt("Enter the city name ");
+    return addressBookArr.filter(contact => contact.city == searchCity);
+}
+
+let searchByState = () => {
+    let searchState = prompt("Enter the state name ");
+    return addressBookArr.filter(contact => contact.state == searchState);
+}
+
 console.log("Welcome to AddressBook Program!!");
 let choice = 0;
 do {
-    console.log("Choose\n1. View Contacts\n2. Add Contact\n3. Edit Contact By name\n4. Delete Contact\n5. Exit");
+    console.log("Choose\n1. View Contacts\n2. Add Contact\n3. Edit Contact By name\n4. Delete Contact\n5. Search Contacts By City\n6. Search Contacts By State\n7. Exit");
     choice = prompt("Enter Your Choice ");
     switch (choice) {
         case "1": viewContacts();
@@ -118,9 +91,13 @@ do {
             break;
         case "4": console.log(deleteContact().toString());
             break;
-        case "5": console.log("Bye!!");
+        case "5": searchByCity();
+            break;
+        case "6": searchByState();
+            break;
+        case "7": console.log("Bye!!");
             break;
         default: console.log("Invalid Choice !!");
     }
 
-} while (choice != 5)
+} while (choice != 7)
